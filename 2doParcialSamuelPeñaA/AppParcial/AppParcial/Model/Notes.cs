2496fdbb3e5b9d18eb73121d0e1cd2dfb1743e16
Samuel Peña <samuel_pena@hotmail.es>
2019-06-22 11:25:11 -0400

    Modelado Completo

---
+++ 2doParcialSamuelPeñaA/AppParcial/AppParcial/Model/Notes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+namespace AppParcial.Model
+{
+    public class Notes
+    {
+        [PrimaryKey, AutoIncrement]
+        public int NotesId { get; set; }
+        public string Contents { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public Notes()
+        {
+            this.CreatedDate = DateTime.Now;
+            this.ModifiedDate = DateTime.Now;
+        }
+    
+    }
+}
---
+++ 2doParcialSamuelPeñaA/AppParcial/AppParcial/ViewModel/NotesViewModel.cs
@@ -0,0 +1,72 @@
+﻿using AppParcial.Model;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppParcial.NotasViewModel
+{
+    class NotesViewModel
+    {
+        private SQLiteConnection conec;
+        private static NotesViewModel instance;
+        public static NotesViewModel Instance
+        {
+            get
+            {
+                if (instance == null) { throw new Exception("Falta inicializar"); }
+                return instance;
+            }
+        }
+
+        public static void Inicializador(String filename) 
+        {
+            if (filename == null) { throw new ArgumentException(); }
+            if (instance != null) { instance.conec.Close(); }
+            instance = new NotesViewModel(filename);
+        }
+
+        private NotesViewModel(String DbPath)
+        {
+            conec = new SQLiteConnection(DbPath);
+            conec.CreateTable<Notes>();
+        }
+        public String EstadoMensaje;
+
+        public int AddNew(string contents)
+        {
+            int result = 0;
+
+            try
+            {
+                result = conec.Insert(new Notes()
+                {
+                    Contents = contents
+                });
+                EstadoMensaje = string.Format("Se ingresó exitosamente la Nota"); 
+            }
+            catch (Exception e)
+            {
+                EstadoMensaje = e.Message;
+            }
+
+            return result;
+        }
+
+        public IEnumerable<Notes> GetAll()
+        {
+            try
+            {
+                return conec.Table<Notes>();
+            }
+            catch (Exception e)
+            {
+                EstadoMensaje = e.Message;
+            }
+            return Enumerable.Empty<Notes>();
+        }
+
+
+    }
+}
